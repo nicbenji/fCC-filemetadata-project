f44baae44b81d3e562d5d48078e13a5c37e90ee6
nicbenji <172269214+nicbenji@users.noreply.github.com>
2025-02-15 17:54:00 +0100

    feat: file can be uploaded to local folder via post req

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const upload = multer({ dest: './public/uploads' });
+require('dotenv').config();
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(_req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (req.file) {
+    const { originalname, mimetype, size } = req.file;
 
+    res.json({
+      name: originalname,
+      type: mimetype,
+      size: size
+    });
+  } else {
+    res.json({ error: 'No file attached' });
+  }
+});
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
